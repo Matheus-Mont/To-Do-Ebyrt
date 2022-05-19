07c5a68cd012d19e85995ada85ac4b652566e2c1
Matheus <mathspa98@gmail.com>
2022-05-19 10:48:25 -0300

    :building_construction: inicial react configs
    creating main react components in its initial state

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from 'react';
+import InputTask from './components/InputTask';
+import TasksList from './components/TasksList';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Lista de tarefas Ebytr</h1>
       </header>
+      <main>
+        <InputTask />
+        <TasksList />
+      </main>
     </div>
   );
 }
